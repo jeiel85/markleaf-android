b19a60c9600748196eb853288e96b9d1abda48a6
박용은 <jeiel85@gmail.com>
2026-05-04 16:55:53 +0900

    chore: bump version to 1.1.16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,8 @@ android {
         applicationId = "com.markleaf.notes"
         minSdk = 26
         targetSdk = 34
-        versionCode = 45
-        versionName = "1.1.15"
+        versionCode = 46
+        versionName = "1.1.16"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
